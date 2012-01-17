7d2ec172662a192504cd355b3f9e23c211165532
Nemo157 <ghostunderscore@gmail.com>
2012-01-17 18:08:12 +1300

    Updated

--- background.js
+++ background.js
@@ -1,1 +1,27 @@
+function loadInjectedCSS(event, path) {
+    var req = new XMLHttpRequest();
+    req.open('GET', path, false);
+    req.send();
+
+    event.source.postMessage({
+        topic: 'LoadedInjectedCSS',
+        data: {
+            css: req.responseText,
+            path: path
+        }
+    });
+}
+
+function onMessage(event) {
+    var message = event.data;
+    if (message.topic === 'LoadInjectedCSS') {
+        var path = message.data;
+        loadInjectedCSS(event, path);
+    }
+}
+
+window.addEventListener('load', function() {
+    opera.extension.onmessage = onMessage;
+}, false);
+
 opera.postError("fogbugz.improv loaded");
